eb3a98ffb9099efb25cec6da04de675f5640c1ac
seongwoo-choi <hibogo789@gmail.com>
2022-07-14 17:35:07 +0900

    add: signed URL

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -8,67 +8,73 @@ import {
 } from "@material-ui/icons";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
   const [movie, setMovie] = useState({});
   const [signedUrl, setSignedUrl] = useState("");
+  const history = useHistory();
 
   useEffect(() => {
     const getMovie = async () => {
       try {
         const res = await axios.get(
-          "https://dev.theotters.net/movies/find/" + item.movieId,
-          {
-            headers: {
-              Authorization:
-                "Bearer " + JSON.parse(localStorage.getItem("user")).token,
-            },
-          }
+            "https://dev.theotters.net/movies/find/" + item.movieId,
+            {
+              headers: {
+                Authorization:
+                    "Bearer " + JSON.parse(localStorage.getItem("user")).token,
+              },
+            }
         );
         // console.log("*********", res.data.movie);
         setMovie(res.data.movie);
-        getMovie();
-
-        const getSignedUrl = async () => {
-          try{
-            const resp  = await axios.get(
-              `https://dev.theotters.net/signedUrl/?movieId=${item.movieId}${ item.isSeries ? "&episodeId="+ item.episodeId : ""}`,{
-              headers: {
-                Authorization:
-                  "Bearer " + JSON.parse(localStorage.getItem("user")).token,
-              }
-            });
-            console.log("*** resp.config.url ***: ", resp.config.url);
-            setSignedUrl(resp.config.url);
-            console.log("*** signedUrl ***: ", signedUrl);
-          } catch (err) {
-            console.log(err);
-          }
-          
-        };
-        getSignedUrl();
-
       } catch (err) {
         console.log(err);
       }
     };
-
+    getMovie();
   }, [item]);
 
+  useEffect(async () => {
+    try {
+      const resp  = await axios.get(
+          `https://dev.theotters.net/signedUrl?movieId=${item.movieId}${ movie.isSeries ? "&episodeId="+ item.id : ""}`,{
+            headers: {
+              Authorization:
+                  "Bearer " + JSON.parse(localStorage.getItem("user")).token,
+            }
+          });
+      setSignedUrl(resp.data.signedUrl);
+    } catch (err) {
+      console.log(err)
+    }
+  }, [item])
+
+
+    const onClick = async () => {
+        history.push("/watch", { signedUrl });
+    }
+
+
+  // console.log("*** signedUrl ***: ", signedUrl);
+  // console.log("*** movie ***: ", movie);
+  // console.log("*** item", item)
+
   return (
-    <Link to={ {pathname: "/watch", movie: signedUrl, } } >
+    // <Link to={ { pathname: "/watch", signedUrl: signedUrl } } >
       <div
         className="listItem"
         style={{ left: isHovered && index * 330 - 50 + index * 2.5 }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
+        onClick={onClick}
       >
         <img src={item?.thumbnail} alt="" />
         {isHovered && (
           <>
-            <video src={item.trailer} autoPlay={true} loop />
+            <video src={signedUrl} autoPlay={true} loop />
             <div className="itemInfo">
               <div className="icons">
                 <PlayArrow className="icon" />
@@ -89,6 +95,6 @@ export default function ListItem({ index, item }) {
           </>
         )}
       </div>
-     </Link>
+     // </Link>
   );
 }
